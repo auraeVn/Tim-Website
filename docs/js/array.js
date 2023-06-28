7c47b976ef601b5396d35b2a87c64cd7c8068a61
Aurelia <aurelia.foucher@outlook.com>
2023-06-28 20:13:07 +0200

    images et wrap

--- docs/js/array.js
+++ docs/js/array.js
@@ -46,10 +46,10 @@ const contenus = [
         description: "2 min, HD, color Found footage <br> Editing and Sound : Timothée Engasser <br> France <br> 2021",
         texte: "Les derniers moments des tours “Démocratie” de Vénissieux.",
         traductionTexte: "The ultimate minutes of the «Démocratie» towers in Venissieux.",
-        image1:  "../images/capture1.png",
-        image2:  "../images/capture2.png",
-        image3:  "../images/capture3.png",
-        image4:  "../images/capture4.png"
+        image1:  "./images/capture1.png",
+        image2:  "./images/capture2.png",
+        image3:  "./images/capture3.png",
+        image4:  "./images/capture4.png"
     },
     {
         id : "Occupation visuelle",
@@ -58,10 +58,10 @@ const contenus = [
         description: "Film documentaire <br> Écriture / Réalisation : Timothée Engasser <br> 15 min <br> HD - N&B <br> Brésil <br> 2015",
         texte: `À travers un important travail d’entretiens, ce travail s’attarde sur le regard que les pixadores posent sur leur ville, leurs pratiques et leurs motivations à peindre dans les rues paulistes. Ce film expose quelques unes des pratiques (escalade, corde, échelle humaine), l’esthétique particulière et l’aspect subversif qui font de la pixação un mouvement unique en son genre.`,
         traductionTexte: `Through an important work of interviews, this work dwells on the look that the pixadores exhibited on their city, their practices and their motivations to paint in the streets of Paulista. This film exposes some of the practices (climbing, rope, human ladder), the particular aesthetics and the subversive aspect that make pixação a unique movement of its kind.`,
-        image1:  "../images/visage.png",
-        image2:  "../images/mur.png",
-        image3:  "../images/dessin.png",
-        image4:  "../images/mur2.png"
+        image1:  "./images/visage.png",
+        image2:  "./images/mur.png",
+        image3:  "./images/dessin.png",
+        image4:  "./images/mur2.png"
        
     },
     {
@@ -73,8 +73,8 @@ const contenus = [
         De l’avènement de l’hygiénisme au XVIIIe siècle à la mise en place d’un véritable marché sécuritaire à partir des années 1980, les auteurs retracent l’histoire de la répression de ce geste d’apparence anodine. Ils cherchent ainsi à comprendre ce qui se joue dans la gouvernance des espaces urbains contemporains et alertent sur ces effacements, signes avant-coureurs de logiques de contrôle bien plus insidieuses, qui ne concernent plus seulement celles et ceux qui écrivent dans la ville.`,
         traductionTexte: `Graffiti is the target of a fierce struggle. Every year, billions of euros are invested globally in safety, cleaning and prevention systems.
         From the advent of hygienism in the 18th century to the establishment of a real safe market from the 1980s, the book traces the history of repression of this seemingly innocuous gesture. Seeking to understand what is at stake in the governance of contemporary urban spaces and warn about these erasures, signs of much more insidious logic of control, which no longer concern only those who write in the city.`,
-        image1:  "../images/antigraphitisme_1.png",
-        image2:  "../images/antigraphitisme_2.png",
+        image1:  "./images/antigraphitisme_1.png",
+        image2:  "./images/antigraphitisme_2.png",
     },
     {
         id : "Dumb City",
@@ -82,10 +82,10 @@ const contenus = [
         description: "Coffret en carton sérigraphié <br> Éditions Terrain Vague <br> Auteurs : Timothée Engasser, Jean-Baptiste Barra <br> 17 x 25,5 x 3 cm <br> Le coffret contient : <br> une carte dépliable présentant plusieurs textes (format A0, impression couleurs offset, recto/verso) <br> 10 photographies argentiques <br> 2 vidéos (8 min. et 9 min.) sur une clé USB (dessin gravé sur une face) <br> des écorces de murs dans un sachet. <br> 100 exemplaires numérotés <br> 2018",
         texte: `Le Coffret Dumb City traite de l’apparition de marques urbaines d’un nouveau genre, abstraites, publiques et multicouches et pose un regard ludique sur l’effacement du graffiti. Une carte retrace l’historique du buff et interroge ses motivations politiques. Grâce à des photos argentiques et des vidéos documentaires, le lecteur découvrira aussi les stigmates que cet effacement acharné laisse sur les murs de Toulouse.`,
         traductionTexte: `The Dumb City Box deals with the appearance of a new kind of urban marks, abstract, public and multi-layered and takes a playful look at the erasure of graffiti. A map traces the history of the buff and questions his political motivations. Thanks to film photos and documentary videos, the reader will also discover the scars that this relentless erasure leaves on the walls of Toulouse.`,
-        image1:  "../images/livre.jpeg",
-        image2:  "../images/livre2.jpeg",
-        image3:  "../images/livre3.jpeg",
-        image4:  "../images/livre4.jpeg"
+        image1:  "./images/livre.jpeg",
+        image2:  "./images/livre2.jpeg",
+        image3:  "./images/livre3.jpeg",
+        image4:  "./images/livre4.jpeg"
     },
     {
         id : "Occupation Visuelle",
@@ -94,17 +94,17 @@ const contenus = [
         description: "Livre photographique <br> Ombu Editions <br> Photographies : Timothée Engasser, Jean-Baptiste Barra, Fabio Vieira <br> Brésil / Chili <br> ISBN : 9791092892093 <br> 160 pages <br> 20x20 cm <br> 2016",
         texte: `Recherche ethnographique visuelle sur la communauté des pixadores à São Paulo. Ce livre se compose d’extraits d’entretiens, de photographies de pixadores, de pixação et de signatures de pixadores.`,
         traductionTexte: `Visual ethnographic research on the community of pixadores in São Paulo. This book features interviews, photographies of pixadores, pixação and signatures of pixadores.`,
-        image1:  "../images/couv_occupa.jpeg",
-        image2:  "../images/couv1.jpeg",
-        image3:  "../images/couv2.jpeg",
-        image4:  "../images/igreja.jpeg",
-        image5:  "../images/igreja2.jpeg",
-        image6:  "../images/jaune.jpeg",
-        image7:  "../images/losange.jpeg",
-        image8:  "../images/centre.jpeg",
-        image9:  "../images/tour.jpeg",
-        image10:  "../images/facade.jpeg",
-        image11:  "../images/tour4.jpeg"
+        image1:  "./images/couv_occupa.jpeg",
+        image2:  "./images/couv1.jpeg",
+        image3:  "./images/couv2.jpeg",
+        image4:  "./images/igreja.jpeg",
+        image5:  "./images/igreja2.jpeg",
+        image6:  "./images/jaune.jpeg",
+        image7:  "./images/losange.jpeg",
+        image8:  "./images/centre.jpeg",
+        image9:  "./images/tour.jpeg",
+        image10: "./images/facade.jpeg",
+        image11: "./images/tour4.jpeg"
     },
     {
         id : "Os invasores",
@@ -113,13 +113,13 @@ const contenus = [
         description: "",
         texte: "",
         traductionTexte:"",
-        image1:  "../images/nyu.png",
-        image2:  "../images/portrait.jpeg",
-        image3:  "../images/tour3.jpeg",
-        image4:  "../images/grille.jpeg",
-        image5:  "../images/grille2.jpeg",
-        image6:  "../images/roots.jpeg",
-        image7:  "../images/pixo.jpeg"
+        image1:  "./images/nyu.png",
+        image2:  "./images/portrait.jpeg",
+        image3:  "./images/tour3.jpeg",
+        image4:  "./images/grille.jpeg",
+        image5:  "./images/grille2.jpeg",
+        image6:  "./images/roots.jpeg",
+        image7:  "./images/pixo.jpeg"
     },
     {
         id : "Effacements",
@@ -128,12 +128,12 @@ const contenus = [
         description: "Numérique <br> Couleur <br> Brésil/Chili/France <br> 2019",
         texte: `Le parti pris radical du pouvoir est de recouvrir les traces illégales par des figures géométriques ou de les remplacer par des gravures. Des groupes de peintres municipaux sont formés et envoyés sur le terrain pour soigner les murs à l’aide de chaux et de jet à haute pression. Peu à peu, les marquages collatéraux de cette thérapie drastique occupent visuellement l’espace urbain. C’est la naissance du buff. L’origine de la démarche ne relève pas de l’histoire de l’art mais plutôt des courants de pensée s sécuritaires et hygiénistes.`,
         traductionTexte: `Power’s strategy is to cover illegal traces with geometric figures or replace them with engravings. Groups of municipal painters are formed and sent to the field to treat the walls with lime and high pressure spray. Little by little, the collateral markings of this drastic therapy visually occupy the urban space. This is the birth of the buff. The origin of the approach does not come from the history of art but rather from currents of thought about security and hygiene.`,
-        image1:  "../images/pano1.jpeg",
-        image2:  "../images/pano2.jpeg",
-        image3:  "../images/pano3.jpeg",
-        image4:  "../images/pano4.jpeg",
-        image5:  "../images/pano5.jpeg",
-        image6:  "../images/pano6.jpeg",
+        image1:  "./images/pano1.jpeg",
+        image2:  "./images/pano2.jpeg",
+        image3:  "./images/pano3.jpeg",
+        image4:  "./images/pano4.jpeg",
+        image5:  "./images/pano5.jpeg",
+        image6:  "./images/pano6.jpeg",
     },
     {
         id : "Oeil pour oeil",
@@ -142,13 +142,13 @@ const contenus = [
         description: "Carte sérigraphiée <br> 80x80 cm <br> 2020",
         texte: `L’élaboration collective de cet objet résulte d’échanges et de réflexions sur les impacts de la vidéosurveillance. À travers de longues balades dans les rues de Toulouse, nous avons observé et relevé cette logistique complexe, in situ. La conception de cette carte a donc été l’occasion d’affirmer une présence collective dans l’espace urbain. En cherchant et en identifiant les caméras, il s’agissait de retourner l’acte de vision, d’installer une contre-surveillance. Comme si veiller sur le dispositif permettait déjà de le déjouer, ne serait-ce qu’un instant.`,
         traductionTexte: `The collective process of this object results from exchanges and reflections on the impacts of video surveillance. Through long walks in the streets of Toulouse, we observed and recorded this logistics complex, in situ. The design of this map was therefore an opportunity to assert a collective presence in the urban space. By looking for and identifying the cameras, it was a question of reversing the act of vision, of installing a counter-surveillance. As if watching over the device already made it possible to thwart it, if only for a moment.`,
-        image1:  "../images/noir.jpeg",
-        image2:  "../images/detail1.jpeg",
-        image3:  "../images/detail2.jpeg",
+        image1:  "./images/noir.jpeg",
+        image2:  "./images/detail1.jpeg",
+        image3:  "./images/detail2.jpeg",
     },
     {
         id : "Bio",
-        titre: "../images/autoportrait.jpeg", 
+        titre: "./images/autoportrait.jpeg", 
         traductionTitre: "",
         description: "FORMATION <br> _Doctorat Recherche-Création ALLPH@ <br> ÉNSAV/UT2J <br><br> _Master Recherche Expérimentation École Nationale Supérieure d’Audiovisuel <br><br> _Master Recherche Institut Pluridisciplinaire pour les Études sur les Amériques de Toulouse <br><br> _Licence Science Politique Université Lyon II",
         texte: `D’abord passé par une licence de Science-Politique, Timothée a ensuite continué sur un Master Recherche de socio-anthropologie qu’il a complété par un Master Recherche/Expérimentation à l’ÉNSAV de Toulouse. Après cela, il a entamé un doctorat entre l’ÉNSAV et de l’Université Toulouse II Jean Jaurès, où il a pu mené une thèse de création-recherche, soutenue en 2019. Celle-ci lui a donné l’occasion d’écrire un essai (édité en 2023, Le passager clandestin) et de réaliser le documentaire Tenacesur le contrôle du graffiti et l’hygiénisme dans l’espace public. Il a ainsi pu publier deux ouvrages photographiques sur ce sujet (Occupation visuelle, Dumb city). Il est actuellement en post-production d’une fiction expérimentale, Il y a un endroit dans la nuit, qui aborde les rapports de celles et ceux qui pratiquent la fête sauvage à leurs lieux, à la musique électronique et à leur mysticisme. En parallèle, il est assistant artistique de (LA)HORDE et lecteur au CNC.`,
